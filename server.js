8fac62680e03619d42b9175acc2379e0d0c50818
badal <badal@badals-MacBook-Air.local>
2024-02-09 17:22:46 +0530

    database connected

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+
 const app = require('./app');
-// console.log(process.env);
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('DB connection established'));
 const port = 3000;
 app.listen(port, () => {
   console.log(`listen on port ${port}`);
