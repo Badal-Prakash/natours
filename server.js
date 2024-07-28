5d6471090b39f39e87442342b50faaa61b05734d
badal <badal@badals-MacBook-Air.local>
2024-07-28 21:45:09 +0530

    mongodbatlas added

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+const LOCAL_DB = process.env.LOCAL_DATABASE;
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-const LOCAL_DB =
-  process.env.LOCAL_DATABASE || 'mongodb://localhost:27017/natours';
-
 mongoose
-  .connect(LOCAL_DB, {
-    useNewUrlParser: true,
+  .connect(DB, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
   })
   .then(() => console.log('DB connection successful!'))
   .catch(err => console.error('DB connection error:', err));
